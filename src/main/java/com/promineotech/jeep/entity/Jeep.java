770d86223b59573c4b1318ebcec91a030e9f8172
fmhagan2 <fmhagan2@gmail.com>
2023-04-29 12:28:50 -0500

    Week 15 edits

--- src/main/java/com/promineotech/jeep/entity/Jeep.java
+++ src/main/java/com/promineotech/jeep/entity/Jeep.java
@@ -1,6 +1,8 @@
 package com.promineotech.jeep.entity;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 
-public class Jeep {
+public class Jeep implements Comparable<Jeep> {
   
  private Long modelPK;
  private JeepModel modelID;
@@ -19,5 +21,22 @@ public class Jeep {
  private int numDoors;
  private int wheelSize;
  private BigDecimal basePrice;
+ 
+ @JsonIgnore   
+ public Long getModelPK() {
+   return modelPK;
+ }
+
+@Override
+public int compareTo(Jeep that) {
+  // @formatter:off
+  return Comparator
+      .comparing(Jeep::getModelID)
+      .thenComparing(Jeep::getTrimLevel)
+      .thenComparing(Jeep::getNumDoors)
+      .compare(this, that);
+  
+  // @formatter:on
+}
   
 }
--- src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -2,20 +2,17 @@ package com.promineotech.jeep.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
-import org.junit.jupiter.api.Disabled;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.jdbc.JdbcTestUtils;
 import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
@@ -56,9 +53,72 @@ class FetchJeepTest extends FetchJeepTestSupport{
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     
     //And: the actual list returned is the same as the expected list
+    List<Jeep> actual = response.getBody();
     List<Jeep> expected = buildExpected();
-    System.out.println(expected);
-    assertThat(response.getBody()).isEqualTo(expected);
+    
+    //actual.forEach(jeep -> jeep.setModelPK(null));
+   
+    assertThat(actual).isEqualTo(expected);
+    // System.out.println(expected);
+
   }
 
+  @Test
+  void testErrorMessageIsReturnedWhenAnUnknownTrimIsSupplied() {
+    //Given: a valid model, trim and URI
+    JeepModel model = JeepModel.WRANGLER;
+    String trim = "Unknown Value";
+    String uri = String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);
+    
+    
+    //When: a connection is made to the URI
+    ResponseEntity<Map<String, Object>> response = 
+        getRestTemplate().exchange(uri, HttpMethod.GET, null,  
+                                   new ParameterizedTypeReference<>() {});
+    
+    //Then: a not found (404) status code is returned
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+    
+    //And: an error message is returned
+    Map<String, Object> error = response.getBody();
+    
+    // @formatter.off
+    assertThat(error)
+      .containsKey("message")
+      .containsEntry("status code", HttpStatus.NOT_FOUND.value())
+      .containsEntry("uri", "/jeeps")
+      .containsKey("timestamp")
+      .containsEntry("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
+    // @formatter.on  
+  }
+  
+//  @ParamTest
+//  void testErrorMessageIsReturnedWhenAnInvalidTrimIsSupplied() {
+//    //Given: a valid model, trim and URI
+//    JeepModel model = JeepModel.WRANGLER;
+//    String trim = "Unknown Value";
+//    String uri = String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);
+//    
+//    
+//    //When: a connection is made to the URI
+//    ResponseEntity<Map<String, Object>> response = 
+//        getRestTemplate().exchange(uri, HttpMethod.GET, null,  
+//                                   new ParameterizedTypeReference<>() {});
+//    
+//    //Then: a not found (404) status code is returned
+//    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+//    
+//    //And: an error message is returned
+//    Map<String, Object> error = response.getBody();
+//    
+//    // @formatter.off
+//    assertThat(error)
+//      .containsKey("message")
+//      .containsEntry("status code", HttpStatus.NOT_FOUND.value())
+//      .containsEntry("uri", "/jeeps")
+//      .containsKey("timestamp")
+//      .containsEntry("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
+//    // @formatter.on  
+//  }
+ 
 }
